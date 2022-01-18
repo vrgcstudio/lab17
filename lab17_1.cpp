59a0076881ac2d3b621464e66f0bd0aa51f193b9
Rapepol <vrgcstudio@gmail.com>
2022-01-18 20:37:49 +0700

    1

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -32,3 +32,53 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double *a, int N , int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << *(a + i);
+        if((i+1) % M == 0) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *a,int N, int M){
+    for(int i = 0; i < N*M; i++){
+        a[i] =  (rand() % 101) * 0.01 ;
+    }
+}
+
+void findRowSum(const double *a, double *b, int N, int M){
+    for(int i = 0 ; i < N ; i++){
+        double sum = 0;
+        
+        for(int j = 0 ; j < M ; j++){
+            sum += *((a + j) + (M * i));
+        }
+        
+        b[i] = sum;
+    }
+}
+
+
+void findColSum(const double *a, double *b, int N, int M)
+{
+    int i = 0, j = 0;
+    b[j] = 0;
+    
+    for(int m = 0 ; m < M ; m++)
+    {
+        i = m;
+        
+        for(int n = 0 ; n < N ; n++)
+        {
+            b[j] += a[i];
+            i += M;
+        }
+        
+        j++;
+        b[j] = 0;
+    }
+}
+
+
